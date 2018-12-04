82d8e26c493d39753224f6e2803e48bb7a2c483f
NikolaBlagojevic38 <blagojevicnikola38@gmail.com>
2018-12-04 01:08:54 +0100

    Updates

---
+++ precache-manifest.d565aac2991c0e6f6be1c2553aa96e2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1e95df4053a5df07fa8",
+    "url": "/my-mernstack-app/static/js/runtime~main.c1e95df4.js"
+  },
+  {
+    "revision": "a968110333088bd4057b",
+    "url": "/my-mernstack-app/static/js/main.a9681103.chunk.js"
+  },
+  {
+    "revision": "d4d91dc88b9440dc8fd6",
+    "url": "/my-mernstack-app/static/js/1.d4d91dc8.chunk.js"
+  },
+  {
+    "revision": "d4d91dc88b9440dc8fd6",
+    "url": "/my-mernstack-app/static/css/1.5cb0d122.chunk.css"
+  },
+  {
+    "revision": "8a52def4965ffb065b1c8c750224f678",
+    "url": "/my-mernstack-app/index.html"
+  }
+];
